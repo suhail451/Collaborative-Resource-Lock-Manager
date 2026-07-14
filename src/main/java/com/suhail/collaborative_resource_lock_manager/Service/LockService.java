40f8fa2ddca2cfa04fa9f39a08868d11ff0f82c4
Suhail Ahmed <arainsuhail451@gmail.com>
2026-07-14 19:33:27 +0500

    Added Lock Acquire Method in service class

--- src/main/java/com/suhail/collaborative_resource_lock_manager/Service/LockService.java
+++ src/main/java/com/suhail/collaborative_resource_lock_manager/Service/LockService.java
@@ -4,8 +4,21 @@ package com.suhail.collaborative_resource_lock_manager.Service;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class LockService  {
+
+    public enum LockResult {
+        ACQUIRED,
+        ALREADY_HELD,
+        NOT_HELD,
+        NOT_OWNER,
+        RENEW
+
+    }
+
     final HistoryService historyService;
     final RedisTemplate<String,String> redisTemplate;
 
@@ -13,6 +26,20 @@ public class LockService  {
         this.historyService = historyService;
         this.redisTemplate = redisTemplate;
     }
+
+    public LockResult acquireLock(String resourceId,String clientId){
+        Boolean acquired=redisTemplate.opsForValue()
+                .setIfAbsent(resourceId,clientId,30, TimeUnit.SECONDS);
+
+    if (acquired){
+        historyService.logEvent(resourceId,clientId,"Acquired",LocalDateTime.now().plusSeconds(30));
+        return LockResult.ACQUIRED;
+    }
+
+        return LockResult.ALREADY_HELD;
+
+    }
+
 
 
 
