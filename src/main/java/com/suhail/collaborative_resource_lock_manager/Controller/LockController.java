e926142e74c37a9074dd8b1983a705d49c81e3bf
Suhail Ahmed <arainsuhail451@gmail.com>
2026-07-15 12:54:52 +0500

    feat: Added renew/{resourceId} endpoint in Lock controller

--- src/main/java/com/suhail/collaborative_resource_lock_manager/Controller/LockController.java
+++ src/main/java/com/suhail/collaborative_resource_lock_manager/Controller/LockController.java
@@ -30,6 +30,19 @@ public class LockController {
 
 
     }
+    @PatchMapping("renew/{resourceId}")
+    public ResponseEntity<String> renew(@PathVariable String resourceId,@RequestBody String clientId){
+        LockService.LockResult result=lockService.renewLock(resourceId,clientId);
+
+        return switch (result){
+            case NOT_HELD -> ResponseEntity.status(NOT_FOUND).body("Lock not found, Already Expired");
+            case NOT_OWNER -> ResponseEntity.status(NOT_ACCEPTABLE).body("This is not your lock");
+            case RENEW -> ResponseEntity.status(OK).body("Lock renewed");
+            default -> ResponseEntity.status(INTERNAL_SERVER_ERROR).body("Unexpected Error");
+
+        };
+    }
+
 
     @DeleteMapping("release/{resourceId}")
     public ResponseEntity<String> delete(@PathVariable String resourceId,@RequestBody String clientId){
@@ -38,7 +51,8 @@ public class LockController {
         return switch(result){
             case RELEASED -> ResponseEntity.status(OK).body("Lock release Succesfully");
             case NOT_OWNER -> ResponseEntity.status(NOT_ACCEPTABLE).body("It is not your Lock");
-            default -> ResponseEntity.status(NOT_FOUND).body("No lock found");
+            case NOT_HELD -> ResponseEntity.status(NOT_FOUND).body("No lock found");
+            default -> ResponseEntity.status(INTERNAL_SERVER_ERROR).body("Unexpected error");
 
         };
 
